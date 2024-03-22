d07c5ec2d8c1a768840f8652094398ec920548e9
Shahrukh Padaniya <shahrukhpadaniya@gmail.com>
2024-03-22 16:20:16 -0400

    Add files via upload

---
+++ learningC++ -1.cpp
@@ -0,0 +1,18 @@
+ // learningC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+//
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	
+	cout << "Enter your name please : "; 
+	cin >> name;
+	cout <<"Nice to meet you " << name << endl;
+
+	return(0);
+}
